80f3957013ea6f85863f048afa16fafadc385ab6
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-02-16 08:29:47 -0800

    Create Reservations component and its css file

---
+++ src/App/Reservations.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import './Reservation.css';
+
+class Reservation extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservation: []
+    }
+  }
+
+  render = () => {
+    return (
+      <h1>Hello World</h1>
+    )
+  }
+}
+
+export default Reservation;
