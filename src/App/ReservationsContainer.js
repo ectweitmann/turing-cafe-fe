a4a7702bb5f89b65c09fe08d3782850321e7479b
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-02-16 08:36:18 -0800

    Create ReservationsContainer component and its css file

---
+++ src/App/ReservationsContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Reservations from './Reservations'
+import './ReservationContainer.css';
+
+const Reservation = ({reservations}) => {
+  return (
+    <div className='resy-container'>
+
+    </div>
+  )
+}
+
+export default ReservationsContainer;
