695d4f8be9bbf1b25df2556303d2f06a06e54e92
Ethan Tweitmann <ethantweitmann@gmail.com>
2022-02-16 08:52:21 -0800

    Add JSX elements

--- src/App/Reservations.js
+++ src/App/Reservations.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import './Reservations.css';
 
-const Reservations = ({reservations}) => {
+const Reservations = ({ name, date, time, numberOfGuests }) => {
   return (
-    <React.Fragment>
-      <h2>{reservations.name}</h2>
-      <p>{reservations.}
-    </React.Fragment>
-  )
+    <section className="resy-card">
+      <h2>{name}</h2>
+      <p>{date}</p>
+      <p>{time}</p>
+      <p>Number of guests: {numberOfGuests}</p>
+      <button>Cancel</button>
+    </section>
+  );
 }
 
 export default Reservations;
